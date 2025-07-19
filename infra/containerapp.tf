0d0c19a8adb31a155a50a1cb61e59f5fa9f975bf
OIgboerika <ignoerikaonochie121@gmail.com>
2025-07-19 10:29:52 +0200

    Add Docker, docker-compose, Azure Terraform IaC, and update README with deployment instructions

---
+++ infra/containerapp.tf
@@ -0,0 +1,55 @@
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "consistlylaw${random_integer.suffix.result}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "main" {
+  name                       = "consistly-env"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+}
+
+resource "azurerm_container_app" "backend" {
+  name                         = "consistly-backend"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "backend"
+      image  = "${azurerm_container_registry.main.login_server}/consistly-backend:latest"
+      cpu    = 0.5
+      memory = "1.0Gi"
+      env {
+        name  = "DATABASE_URL"
+        value = "postgresql://pgadmin:${random_password.postgres.result}@${azurerm_postgresql_flexible_server.main.fqdn}:5432/consistlydb"
+      }
+    }
+    scale {
+      min_replicas = 1
+      max_replicas = 2
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 8000
+    transport        = "auto"
+  }
+
+  registry {
+    server               = azurerm_container_registry.main.login_server
+    username             = azurerm_container_registry.main.admin_username
+    password_secret_name = "acr-password"
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.main.admin_password
+  }
+} 
